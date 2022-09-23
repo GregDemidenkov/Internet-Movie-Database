17b1a09d1170aee80fdd33ac42f4cc07c314c590
GregDemidenkov <greg.demidenkov@yandex.ru>
2022-09-24 02:12:47 +0300

    Fix slider-arrows, add fonts, change token

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,6 +1,7 @@
 const token = 'b35699f3-c603-42ae-96bc-590164f9c971'
+const token_1 = '6e0c4cd5-64e2-412d-ba16-21a38ab9e342'
 
 export const headers = {
-  'X-API-KEY': token,
+  'X-API-KEY': token_1,
   'Content-Type': 'application/json',
 };
